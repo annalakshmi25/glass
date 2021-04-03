f88023506b2dd8668690882e5f63e890bdf492b7
annalakshmi25 <annalakshmiv25@gmail.com>
2021-04-03 07:39:22 +0530

    edited

--- glass/settings.py
+++ glass/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'k_p_bp2my9d&v6xfr%^p6)xk7ie)1u0ma@gwa-roau7skd)3ud'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.220.178.226','localhost']
+ALLOWED_HOSTS = ['3.142.225.158','localhost']
 
 
 # Application definition
